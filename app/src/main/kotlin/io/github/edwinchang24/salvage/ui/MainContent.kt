eeb670bcb58005697c29626a5397b4f6ec249b7c
Edwin Chang <edwinchang2024@gmail.com>
2023-07-30 11:06:02 -0700

    feat: make header hide on scroll

--- app/src/main/kotlin/io/github/edwinchang24/salvage/ui/MainContent.kt
+++ app/src/main/kotlin/io/github/edwinchang24/salvage/ui/MainContent.kt
@@ -9,20 +9,30 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.navigation.NavController
 import io.github.edwinchang24.salvage.feature.itemediting.newitem.navigateToNewItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainContent(rootNavController: NavController, content: @Composable (contentPadding: PaddingValues) -> Unit) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = "Salvage") }) },
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Salvage") },
+                scrollBehavior = scrollBehavior
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = rootNavController::navigateToNewItem) {
                 Icon(Icons.Default.Add, contentDescription = "New item")
             }
         },
-        content = content
+        content = content,
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     )
 }
